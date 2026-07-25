632d244ad3ce1111980ae7536940fa6f0446e06d
itaisinger <105855317+itaisinger@users.noreply.github.com>
2026-07-25 09:41:14 +0300

    score counter

--- finger-game/Scenes/score_counter/score_counter.gd
+++ finger-game/Scenes/score_counter/score_counter.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export var start_val = 0
+@export var rolf_range = 15
 
 var current_remain = 0.0
 var count = 0;
+var spd = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,20 +13,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	current_remain -= delta
+	current_remain -= delta*spd
 	if(current_remain <= 0):
 		#progress
 		current_remain += 1
 		count -= 1
-		
+		print("tick")
+		var vol = randf_range(0.7,1.3) 
 		#jokes
-		var vol = 1.0
-		var pitch = 1
-		if(randi_range(0,5) == 2): count += 2
-		if(randi_range(0,5) == 2): current_remain *= randf_range(0.3,0.6) 
-		if(randi_range(0,5) == 2): current_remain *= randf_range(2,3) 
-		if(randi_range(0,5) == 2): vol *= 1.3 
-		if(randi_range(0,5) == 2): pitch *= 1.3 
+		var pitch = randf_range(0.7,1.3) 
+		if(randi_range(0,rolf_range) == 2): count += 2
+		if(randi_range(0,rolf_range) == 2): current_remain *= randf_range(0.3,0.6) 
+		if(randi_range(0,rolf_range) == 2): current_remain *= randf_range(2,3) 
+		if(randi_range(0,rolf_range) == 2): vol *= 1.5
+		if(randi_range(0,rolf_range) == 2): pitch *= 1.5 
 		
 		#sfx
+		$tickSfx.play();
+		
+func done():
+	print("win")
+	
+func disable():
+	spd = 0
 		
